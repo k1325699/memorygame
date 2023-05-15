5cb7fc30fade65d80870c9f89a01f90fd79e8331
k1325699 <k1325699@gmail.com>
2023-05-15 16:47:53 +0800

    fix: 調整loading卡片大小

--- components/Loading.jsx
+++ components/Loading.jsx
@@ -1,7 +1,7 @@
 const Loading = () => {
   return (
     <div className="w-full h-full flex justify-center items-center">
-      <div className=" w-[30%]  max-w-[300px] bg-white rounded-[5%] rotate-[30deg] relative before:content-[''] before:block  before:pb-[150%] animate-spin"></div>
+      <div className=" w-[30%]  max-w-[200px] bg-white rounded-[5%] rotate-[30deg] relative before:content-[''] before:block  before:pb-[150%] animate-spin"></div>
     </div>
   );
 };
